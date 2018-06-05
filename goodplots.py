3853d7e756a1b514b109ef5747f74d36d56edcf7
Sam Ireland <sam.ireland.uk@gmail.com>
2018-06-05 19:42:32 +0100

    Series representation

--- goodplots.py
+++ goodplots.py
@@ -11,13 +11,32 @@ class Series:
     TYPES = ("line", "bar", "scatter")
 
     def __init__(self, data, type="line", name=""):
-        if len(data[0]) != 2:
-            self._data = list(enumerate(data, start=1))
-        else:
-            if any(len(datum) != 2 for datum in data):
-                raise ValueError(f"Data for series is not structured correctly")
-            self._data = data
+        data = list(data)
+        try:
+            iter(data[0])
+            assert len(data[0]) == 2
+        except:
+            data = list(enumerate(data, start=1))
+
+        if any(len(datum) != 2 for datum in data):
+            raise ValueError(f"Data for series is not structured correctly")
+        self._data = data
+
         if type not in self.TYPES:
             raise ValueError(f"Valid types are {self.TYPES} - not '{type}'")
         self._type = type
         self._name = name
+
+
+    def __repr__(self):
+        return "<{} Series {}{}>".format(
+         self._type.title(),
+         "'{}' ".format(self._name) if self._name else "",
+         self._data)
+
+
+    def __str__(self):
+        return "<{} Series {}({} data points)>".format(
+         self._type.title(),
+         "'{}' ".format(self._name) if self._name else "",
+         len(self._data))
--- tests/unit/test_series.py
+++ tests/unit/test_series.py
@@ -10,8 +10,8 @@ class SeriesCreationTests(TestCase):
 
 
     def test_can_enumerate_basic_iterable(self):
-        series = Series(["A", "B"])
-        self.assertEqual(series._data, [(1, "A"), (2, "B")])
+        series = Series([10, 20])
+        self.assertEqual(series._data, [(1, 10), (2, 20)])
 
 
     def test_data_must_be_structured_correctly(self):
@@ -34,3 +34,29 @@ class SeriesCreationTests(TestCase):
     def test_can_create_series_with_name(self):
         series = Series([[0, 0], [1, 1], [2, 4]], name="series 2")
         self.assertEqual(series._name, "series 2")
+
+
+
+class SeriesReprTests(TestCase):
+
+    def test_series_repr(self):
+        series = Series([[0, 0], [1, 1], [2, 4]])
+        self.assertEqual((repr(series)), "<Line Series [[0, 0], [1, 1], [2, 4]]>")
+
+
+    def test_series_repr_with_name(self):
+        series = Series([[0, 0], [1, 1], [2, 4]], name="S2")
+        self.assertEqual((repr(series)), "<Line Series 'S2' [[0, 0], [1, 1], [2, 4]]>")
+
+
+
+class SeriesStrTests(TestCase):
+
+    def test_series_str(self):
+        series = Series([[0, 0], [1, 1], [2, 4]])
+        self.assertEqual((str(series)), "<Line Series (3 data points)>")
+
+
+    def test_series_str_with_name(self):
+        series = Series([[0, 0], [1, 1], [2, 4]], name="S2")
+        self.assertEqual((str(series)), "<Line Series 'S2' (3 data points)>")
