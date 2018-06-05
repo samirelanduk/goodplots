2adde8ac55ffaf7e19d8250c2d858cdd25396e84
Sam Ireland <sam.ireland.uk@gmail.com>
2018-06-05 19:27:30 +0100

    Series initialisation

--- goodplots.py
+++ goodplots.py
@@ -8,5 +8,16 @@ class Chart:
 
 class Series:
 
-    def __init__(self, data):
-        self._data = data
+    TYPES = ("line", "bar", "scatter")
+
+    def __init__(self, data, type="line", name=""):
+        if len(data[0]) != 2:
+            self._data = list(enumerate(data, start=1))
+        else:
+            if any(len(datum) != 2 for datum in data):
+                raise ValueError(f"Data for series is not structured correctly")
+            self._data = data
+        if type not in self.TYPES:
+            raise ValueError(f"Valid types are {self.TYPES} - not '{type}'")
+        self._type = type
+        self._name = name
--- tests/unit/test_series.py
+++ tests/unit/test_series.py
@@ -4,5 +4,33 @@ from goodplots import Series
 class SeriesCreationTests(TestCase):
 
     def test_can_create_series(self):
-        series = Series([[1, "A"], [2, "B"]])
-        self.assertEqual(series._data, [[1, "A"], [2, "B"]])
+        series = Series([[0, 0], [1, 1], [2, 4]])
+        self.assertEqual(series._data, [[0, 0], [1, 1], [2, 4]])
+        self.assertEqual(series._type, "line")
+
+
+    def test_can_enumerate_basic_iterable(self):
+        series = Series(["A", "B"])
+        self.assertEqual(series._data, [(1, "A"), (2, "B")])
+
+
+    def test_data_must_be_structured_correctly(self):
+        with self.assertRaises(ValueError):
+            Series([[10, "A"], ["B"]])
+        with self.assertRaises(ValueError):
+            Series([[10, "A"], ["A", "B", "C"]])
+
+
+    def test_can_create_series_with_type(self):
+        series = Series([[0, 0], [1, 1], [2, 4]], type="bar")
+        self.assertEqual(series._type, "bar")
+
+
+    def test_can_create_series_type_must_be_valid(self):
+        with self.assertRaises(ValueError):
+            Series([[0, 0], [1, 1], [2, 4]], type="blib")
+
+
+    def test_can_create_series_with_name(self):
+        series = Series([[0, 0], [1, 1], [2, 4]], name="series 2")
+        self.assertEqual(series._name, "series 2")
