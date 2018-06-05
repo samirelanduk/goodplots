7723ba1510f80deba0bf81011e2026ddeed85b8f
Sam Ireland <sam.ireland.uk@gmail.com>
2018-06-05 19:52:06 +0100

    Chart creation

--- goodplots.py
+++ goodplots.py
@@ -1,8 +1,20 @@
 
 class Chart:
 
-    def __init__(self):
-        self._series = []
+    def __init__(self, series=(), title=""):
+        try:
+            iter(series)
+        except TypeError:
+            raise ValueError("Chart must be created with iterable of Series")
+        for s in series:
+            if not isinstance(s, Series):
+                raise TypeError(f"{s} is not a Series object")
+        self._series = list(series)
+        self._title = title
+
+
+    def __repr__(self):
+        return f"<Chart ({len(self._series)} series)>"
 
 
 
@@ -17,11 +29,9 @@ class Series:
             assert len(data[0]) == 2
         except:
             data = list(enumerate(data, start=1))
-
         if any(len(datum) != 2 for datum in data):
             raise ValueError(f"Data for series is not structured correctly")
         self._data = data
-
         if type not in self.TYPES:
             raise ValueError(f"Valid types are {self.TYPES} - not '{type}'")
         self._type = type
--- tests/unit/test_charts.py
+++ tests/unit/test_charts.py
@@ -1,8 +1,41 @@
 from unittest import TestCase
-from goodplots import Chart
+from unittest.mock import Mock, patch
+from goodplots import Chart, Series
 
-class ChartCreationTests(TestCase):
+class ChartTest(TestCase):
+
+    def setUp(self):
+        self.series = [Mock(Series), Mock(Series), Mock(Series)]
+
+
+
+class ChartCreationTests(ChartTest):
 
     def test_can_create_empty_chart(self):
         chart = Chart()
         self.assertEqual(chart._series, [])
+
+
+    def test_can_create_chart_with_series(self):
+        chart = Chart(self.series)
+        self.assertEqual(chart._series, self.series)
+
+
+    def test_series_must_be_iterable_of_series(self):
+        with self.assertRaises(ValueError):
+            Chart(100)
+        with self.assertRaises(TypeError):
+            Chart(["1", "2"])
+
+
+    def test_can_create_chart_with_title(self):
+        chart = Chart(self.series, title="Chart Title")
+        self.assertEqual(chart._title, "Chart Title")
+
+
+
+class ChartReprTests(ChartTest):
+
+    def test_chart_repr(self):
+        chart = Chart(self.series)
+        self.assertEqual(repr(chart), "<Chart (3 series)>")
